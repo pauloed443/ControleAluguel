82fbf5b9e7cd1a4e297f2acfbeb478f5e2ffae96
Unknown <pauloed443@gmail.com>
2018-08-15 00:49:42 -0300

    Criação da Conexao Singleton

---
+++ dao/Conexao.php
@@ -0,0 +1,35 @@
+<?php 
+
+/**
+ * 
+ */
+class Conexao {
+
+	private static $host;
+	private static $dbname;
+	private static $user;
+	private static $pass;
+	public static $connection;
+
+	function __construct()
+	{
+		self::$host = 'localhost';
+		self::$dbname = 'leidebrink';
+		self::$user = 'root';
+		self::$pass = '';
+	}
+
+	public static function getConnection() {
+		if (!isset(self::$connection)) {
+			try {
+				self::$connection = new PDO('mysql:host='.self::$host.';dbname='.self::$dbname, self::$user, self::$pass);
+			} catch (PDOException $e) {
+				echo "Erro conexão".$e->getMessage();
+			}
+		}
+		return self::$connection;
+	}
+
+}
+
+
